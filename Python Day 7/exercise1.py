8f2278b78df3f481ceb8258efeaadabc11abe77a
Nick Mwangemi <nickmwangemi@gmail.com>
2020-05-27 18:21:02 +0300

    Day 7 exercises and notes

---
+++ Python Day 7/exercise1.py
@@ -0,0 +1,9 @@
+# weight converter - kilos to pounds
+# ask the user for their weight
+# ask which unit of weight they'd like to convert to
+# convert the weight then output with appropriate units i.e 70 kilos or 200 pounds
+ 
+# from kilos to pounds
+# /0.45
+# from pounds to kilos
+# *0.45
